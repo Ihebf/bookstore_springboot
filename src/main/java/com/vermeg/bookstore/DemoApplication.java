8a2460485555750c4f186d7d32b57a6efce70605
Ihebf <iheb_farhat@hotmail.com>
2020-11-29 14:02:39 +0100

    Rest Api & Jpa entities & Connection hibernate

--- src/main/java/com/vermeg/bookstore/DemoApplication.java
+++ src/main/java/com/vermeg/bookstore/DemoApplication.java
@@ -22,17 +22,17 @@ public class DemoApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         bookRepository.save(new Book(null, "title1","author1",100.0,"releaseDate1"));
         bookRepository.save(new Book(null, "title2","author2",200.0,"releaseDate2"));
         bookRepository.save(new Book(null, "title3","author3",300.0,"releaseDate3"));
-        bookRepository.findAll().forEach(book -> System.out.println(book));
+        bookRepository.findAll().forEach(System.out::println);
 
         orderRepository.save(new Order(null,5,9));
         orderRepository.save(new Order(null,1,3));
         orderRepository.save(new Order(null,10,5));
-        orderRepository.findAll().forEach(order -> System.out.println(order));
+        orderRepository.findAll().forEach(System.out::println);
 
     }
 }
